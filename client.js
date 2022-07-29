558065186ba238d17fd13b28cf91749f950bdeda
michaelfong2017 <michaelfong2017@gmail.com>
2022-07-29 15:32:28 +0800

    Headlocked panels (follows horizontally and vertically, follows horizontally only)

--- client.js
+++ client.js
@@ -1,12 +1,72 @@
-import { ReactInstance } from "react-360-web";
+import {ReactInstance, Surface} from 'react-360-web';
 import HLSVideoPlayer from "./HLSVideoPlayer";
 import MouseZoomPanCameraController from "./MouseZoomPanCameraController";
 
+function rotateByQuaternion(v: Vec3, q: Quaternion) {
+  // Optimized implementation of Hamiltonian product, similar to Unity's
+  // internal implementation
+  const qx = q[0];
+  const qy = q[1];
+  const qz = q[2];
+  const qw = q[3];
+  const vx = v[0];
+  const vy = v[1];
+  const vz = v[2];
+  const qx2 = qx + qx;
+  const qy2 = qy + qy;
+  const qz2 = qz + qz;
+
+  const xx2 = qx * qx2;
+  const yy2 = qy * qy2;
+  const zz2 = qz * qz2;
+  const xy2 = qx * qy2;
+  const xz2 = qx * qz2;
+  const yz2 = qy * qz2;
+  const wx2 = qw * qx2;
+  const wy2 = qw * qy2;
+  const wz2 = qw * qz2;
+
+  v[0] = (1 - yy2 - zz2) * vx + (xy2 - wz2) * vy + (xz2 + wy2) * vz;
+  v[1] = (xy2 + wz2) * vx + (1 - xx2 - zz2) * vy + (yz2 - wx2) * vz;
+  v[2] = (xz2 - wy2) * vx + (yz2 + wx2) * vy + (1 - xx2 - yy2) * vz;
+}
+
 function init(bundle, parent, options = {}) {
+  const horizontalPanel = new Surface(300, 300, Surface.SurfaceShape.Flat);
+  const hvPanel = new Surface(300, 300, Surface.SurfaceShape.Flat);
+
+  horizontalPanel.setAngle(0, -0.5);
+
+
   const r360 = new ReactInstance(bundle, parent, {
     // Add custom options here
     fullScreen: true,
     customVideoPlayers: [HLSVideoPlayer],
+    frame: () => {
+      const cameraQuat = r360.getCameraQuaternion();
+      const cameraDirection = [0, 0, -1];
+
+      // cameraDirection will point out from the view of the camera,
+      // we can use it to compute surface angles
+      rotateByQuaternion(cameraDirection, cameraQuat);
+      const cx = cameraDirection[0];
+      const cy = cameraDirection[1];
+      const cz = cameraDirection[2];
+      const horizAngle = Math.atan2(cx, -cz);
+      const vertAngle = Math.asin(cy / Math.sqrt(cx * cx + cy * cy + cz * cz));
+      horizontalPanel.setAngle(horizAngle, -0.5);
+      hvPanel.setAngle(horizAngle, vertAngle);
+      
+      const DEFAULT_RADIUS = 4
+      const RADIUS_SCALE = 225
+      hvPanel.setRadius(DEFAULT_RADIUS * RADIUS_SCALE)
+      const DEFAULT_DENSITY = 4680
+      const DENSITY_SCALE = 1/2
+      var d = 1 / DENSITY_SCALE * DEFAULT_DENSITY / (Math.abs(r360.getCameraPosition()[2]) / 16 / RADIUS_SCALE + 0.25) / 4
+      console.log(d)
+      // horizontalPanel.setDensity(d, d)
+      hvPanel.setDensity(d, d)
+    },
     ...options,
   });
 
@@ -17,6 +77,9 @@ function init(bundle, parent, options = {}) {
     }),
     r360.getDefaultSurface()
   );
+
+  r360.renderToSurface(r360.createRoot('HorizontalPanel'), horizontalPanel);
+  r360.renderToSurface(r360.createRoot('HVPanel'), hvPanel);
 
   removeEventListener("resize", r360._onResize);
 
--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-import React from 'react';
+import React from "react";
 import {
   AppRegistry,
   View,
   StyleSheet,
   asset,
-} from 'react-360';
-import VideoModule from 'VideoModule';
-import * as Environment from 'Environment';
+  Text,
+  VrButton,
+} from "react-360";
+import VideoModule from "VideoModule";
+import * as Environment from "Environment";
 
-const VIDEO_PLAYER = 'hls_video';
-const VIDEO_SOURCE =[
+const VIDEO_PLAYER = "hls_video";
+const VIDEO_SOURCE = [
   // {
-  //   url: asset('video_dash_mp4/video_stream.mpd').uri, 
+  //   url: asset('video_dash_mp4/video_stream.mpd').uri,
   //   fileFormat: 'mp4',
   // },
   // {
-  //   url: asset('video_dash_webm/video_stream.mpd').uri, 
+  //   url: asset('video_dash_webm/video_stream.mpd').uri,
   //   fileFormat: 'webm',
   // },
   {
-    url: 'https://api.robocore.ai/stream/stream/llhls.m3u8', 
-    fileFormat: 'webm',
-  }
+    url: "https://api.robocore.ai/stream/stream/llhls.m3u8",
+    fileFormat: "webm",
+  },
 ];
 
 class react_vr extends React.Component {
   componentDidMount() {
-    VideoModule.createPlayer(VIDEO_PLAYER);
+    window.player360 = VideoModule.createPlayer(VIDEO_PLAYER);
+    console.log("yoooooooooooooooooooooooooooooooooooooooooo\n\n\n");
+    console.log(window.player360);
     VideoModule.play(VIDEO_PLAYER, {
       source: VIDEO_SOURCE,
-      stereo: '2D',
+      stereo: "2D",
     });
 
     Environment.setBackgroundVideo(VIDEO_PLAYER);
@@ -40,4 +44,44 @@ class react_vr extends React.Component {
   }
 }
 
-AppRegistry.registerComponent('react_vr', () => react_vr);
+const _pressed = () => {
+  console.log(window.player360);
+  window.player360.pause();
+};
+
+const HorizontalPanel = () => (
+  <View style={styles.panel}>
+    <VrButton onClick={_pressed} style={styles.greetingBox}>
+      <Text style={styles.panelText}>{"Follows Horizontally"}</Text>
+    </VrButton>
+  </View>
+);
+
+const HVPanel = () => (
+  <View style={styles.panel}>
+    <VrButton onClick={_pressed} style={styles.greetingBox}>
+      <Text style={styles.panelText}>
+        {"Follows Horizontally\nand Vertically"}
+      </Text>
+    </VrButton>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  panel: {
+    width: 300,
+    height: 300,
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  panelText: {
+    color: "#000000",
+    fontSize: 30,
+    textAlign: "center",
+  },
+});
+
+AppRegistry.registerComponent("react_vr", () => react_vr);
+AppRegistry.registerComponent("HorizontalPanel", () => HorizontalPanel);
+AppRegistry.registerComponent("HVPanel", () => HVPanel);
